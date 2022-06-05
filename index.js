287374138c1335057a5217fd0e77affeda1d753f
Naomi Cole <naomisarahcole@gmail.com>
2022-06-05 19:26:23 +1000

    1st test passed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file!
+// 
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0,2);
+};

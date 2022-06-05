f8a2b30127b9a51920cb07ad9d82886db1f4ea32
Naomi Cole <naomisarahcole@gmail.com>
2022-06-05 19:36:49 +1000

    Passing 2 tests

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
 const returnFirstTwoDrivers = function (drivers) {
     return drivers.slice(0,2);
 };
+
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(2,5);
+};
+
+const selectingDrivers = [returnLastTwoDrivers() + returnFirstTwoDrivers()];
